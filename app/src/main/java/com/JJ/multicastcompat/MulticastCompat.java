05377965aa309ffef38d38fabf35897a6bb8c27e
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-12-20 11:50:01 +0100

    moved interfaces inside multicastcompat class

--- app/src/main/java/com/JJ/multicastcompat/MulticastCompat.java
+++ app/src/main/java/com/JJ/multicastcompat/MulticastCompat.java
@@ -25,6 +25,20 @@ public class MulticastCompat {
 		setupResolveProxy();
 	}
 	
+	public interface ResolveListener {
+		void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode);
+		void onServiceResolved(NsdServiceInfo serviceInfo);
+	}
+	
+	public interface DiscoveryListener {
+		void onStartDiscoveryFailed(String serviceType, int errorCode);
+		void onStopDiscoveryFailed(String serviceType, int errorCode);
+		void onDiscoveryStarted(String serviceType);
+		void onDiscoveryStopped(String serviceType);
+		void onServiceFound(NsdServiceInfo serviceInfo);
+		void onServiceLost(NsdServiceInfo serviceInfo);
+	}
+	
 	private void setupDiscoveryProxy() {
 		discoveryListener = new NsdManager.DiscoveryListener() {
 			@Override
--- app/src/main/java/com/JJ/multicastcompat/ResolveListener.java
+++
@@ -1,8 +0,0 @@
-package com.JJ.multicastcompat;
-
-import android.net.nsd.NsdServiceInfo;
-
-public interface ResolveListener {
-	void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode);
-	void onServiceResolved(NsdServiceInfo serviceInfo);
-}
